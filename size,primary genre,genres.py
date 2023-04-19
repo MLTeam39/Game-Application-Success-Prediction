0da6ebac8d6b09b1c038be93f1b72ceb7dcc306d
TasneemMagdy2002 <131301862+TasneemMagdy2002@users.noreply.github.com>
2023-04-19 21:49:54 +0200

    Add files via upload

---
+++ size,primary genre,genres.py
@@ -0,0 +1,122 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[109]:
+
+
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn import linear_model
+from sklearn import metrics
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder
+from sklearn.preprocessing import StandardScaler
+import datetime as dt
+from sklearn.linear_model import LinearRegression
+import itertools 
+import functools
+from sklearn import preprocessing
+from sklearn.preprocessing import MaxAbsScaler
+
+#Load data
+data = pd.read_csv("C:\\Users\\tasne\\Downloads\\games-regression-dataset.csv")
+
+
+##########################################
+#Drop the rows that contain missing values
+data.dropna(how='any',inplace=True)
+data.drop_duplicates()
+
+##########################################
+_data=data.iloc[:,:]
+X=data.iloc[:,0:16] #Features
+Y=data['Average User Rating'] #Label
+
+##########################################
+#Split the data to training and testing sets                          
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size = 0.20,shuffle=True)
+
+##########################################
+##feature scaling
+##'Size'
+Size_new=pd.read_csv("C:\\Users\\tasne\\Downloads\\games-regression-dataset.csv",usecols=['Size'])
+
+#print(data['Size'].isna().sum())
+
+tempFeat = np.array(Size_new['Size']).reshape(-1,1)
+scaler = MaxAbsScaler()
+scaler.fit(tempFeat)
+scaledFeat = scaler.transform(tempFeat)
+Size_new['Size'] = scaledFeat.reshape(1,-1)[0]
+data_cpycpy=data.drop(['Size'],axis=1)
+data= pd.DataFrame(data_cpycpy)
+data['Size'] = Size_new['Size']
+
+##########################################
+##'Primary Genre'
+##'Genres'
+PrimaryGenre =pd.read_csv("C:\\Users\\tasne\\Downloads\\games-regression-dataset.csv",usecols=['Primary Genre'])
+Genres =pd.read_csv("C:\\Users\\tasne\\Downloads\\games-regression-dataset.csv",usecols=['Genres'])
+
+count =0
+
+for i  in range(len(PrimaryGenre.index)):
+    if(PrimaryGenre.iloc[i].to_string().split(' ')[5].__eq__((Genres.iloc[i].to_string()).split(', ')[0].split(' ')[4])):
+            count=count+1
+  
+        
+flag="%.2f"%(count/len(PrimaryGenre)*100)    
+
+if (float(flag)>=99):
+    data = data.drop(['Primary Genre'],axis=1)
+    
+########################################## 
+##'Genres'
+
+output = data['Genres'].str.get_dummies(sep=', ')
+
+#data=pd.concat(output)
+#data=data(output.columns.values.tolist())
+
+#data["Action","Adventure","Board","Books","Business","Card","Casino","Casual","Education","Entertainment" ," Reference","Role", "Playing","Simulation", "Social Networking","Sports","Strategy","Travel","Trivia","Utilities","Word"]=output
+
+
+print(output)    
+
+
+
+data.head ()
+
+
+# In[ ]:
+
+
+
+   
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
