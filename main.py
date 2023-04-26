7c3ced97849c2169cb07c1499b99408f8e8a1493
Radwa-Goudah <131301780+Radwa-Goudah@users.noreply.github.com>
2023-04-26 22:02:16 +0200

    Description

--- main.py
+++ main.py
@@ -1,20 +1,33 @@
 ##################################All Packages##################################
+import nltk
+import re
 import numpy as np
 import pandas as pd
 import statistics
 import matplotlib.pyplot as plt
 import preprocessingFunctions as preFun
 import testPreprocessing as testPre
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+from nltk.stem import WordNetLemmatizer
 from sklearn import linear_model
 from sklearn import metrics
+from collections import Counter
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import PolynomialFeatures
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.feature_extraction.text import CountVectorizer
 
-
+DesCol = []
 filledColumns = {}
+nltk.download('wordnet')
+cv = CountVectorizer()
+stemmer = PorterStemmer()
+stop_words = set(stopwords.words("english"))
+lemmatizer = WordNetLemmatizer()
 
 ##################################Loading Data##################################
 data = pd.read_csv('games-regression-dataset.csv')
@@ -75,6 +88,24 @@ for row in X_train['In-app Purchases']:
 X_train['In-app Purchases'] = newCol
 # print(X_train['In-app Purchases'])
 
+##################################Description##################################
+for row in X_train['Description']:
+    row = word_tokenize(row)
+    row = [preFun.remove_NewLine(i) for i in row]
+    row = [i for i in row if i == re.sub(r'//', '', i)]
+    row = [i for i in row if i == re.sub(r'https', '', i)]
+    row = [re.sub(r'[^a-zA-Z0-9\s]+', '', preFun.remove_punc(i)) for i in row]
+    row = [preFun.remove_numbers(i) for i in row]
+    row = [word for word in row if word not in stop_words]
+    row = [i for i in row if i != '']
+    row = [stemmer.stem(word) for word in row]
+    row = [lemmatizer.lemmatize(word, pos='v') for word in row]
+    # length = len(set(row))
+    # row =  cv.fit_transform(row)
+    # DesCol.append(row.count_nonzero())
+    DesCol.append(len(set(row)))
+
+X_train = preFun.feature_scaling(X_train['Description'], DesCol)
 ##################################Developer##################################
 # Feature Encoding
 # print(X_train['Developer'])
@@ -164,10 +195,10 @@ for genre in genresFreq.keys():
 # print('genres Frequency : \n', genresFreq)
 
 eachRow = []
-i=1
+i = 1
 for row in X_train['Genres']:
     eachRow.append(genresFreq[genresRows[i]].sum())
-    i+=1
+    i += 1
 
 X_train['Genres'] = eachRow
 # print(X_train['Genres'])
@@ -266,7 +297,6 @@ X_test = testPre.primary_test(X_test)
 X_test = testPre.genres_test(X_test, genresFreq, genres)
 X_test = testPre.dates_test(X_test)
 
-
 X_test = testPre.scaler(X_test, 'Developer')
 X_test = testPre.scaler(X_test, 'Price')
 X_test = testPre.scaler(X_test, 'Size')
@@ -309,27 +339,27 @@ X_test = X_test[top_features]
 X_test = X_test.reindex(columns=X_train.columns)
 
 """Linear Reg"""
-for col in X_train:
-    model = LinearRegression()
-    X = np.expand_dims(X_train[col], axis=1)
-    Y = np.expand_dims(Y_train, axis=1)
-    X_test_col = np.expand_dims(X_test[col], axis=1)
-    model.fit(X, Y)
-    y_pred = model.predict(X_test_col)
-
-    print('-Linear Regression Using ',col)
-    print('Mean Square Error', metrics.mean_squared_error(Y_test, y_pred))
-    print('Accuracy', "%.4f" % (metrics.r2_score(Y_test, y_pred)), '\n')
-
-    plt.figure(figsize=(4, 3))
-    ax = plt.axes()
-    ax.scatter(X, Y)
-    ax.plot(X_test_col, y_pred,color='red')
-
-    ax.set_xlabel(col)
-    ax.set_ylabel('Average User Rating')
-    ax.axis('tight')
-    plt.show()
+# for col in X_train:
+#     model = LinearRegression()
+#     X = np.expand_dims(X_train[col], axis=1)
+#     Y = np.expand_dims(Y_train, axis=1)
+#     X_test_col = np.expand_dims(X_test[col], axis=1)
+#     model.fit(X, Y)
+#     y_pred = model.predict(X_test_col)
+#
+#     print('-Linear Regression Using ',col)
+#     print('Mean Square Error', metrics.mean_squared_error(Y_test, y_pred))
+#     print('Accuracy', "%.4f" % (metrics.r2_score(Y_test, y_pred)), '\n')
+#
+#     plt.figure(figsize=(4, 3))
+#     ax = plt.axes()
+#     ax.scatter(X, Y)
+#     ax.plot(X_test_col, y_pred,color='red')
+#
+#     ax.set_xlabel(col)
+#     ax.set_ylabel('Average User Rating')
+#     ax.axis('tight')
+#     plt.show()
 
 """Multiple Reg"""
 
@@ -340,7 +370,6 @@ y_pred = regr.predict(X_test)
 print('-Multiple Regression:')
 print('Mean Square Error', metrics.mean_squared_error(np.asarray(Y_test), y_pred))
 print('Accuracy', "%.4f" % (metrics.r2_score(Y_test, y_pred)), '\n')
-
 
 """Polynomial Reg"""
 
@@ -365,24 +394,24 @@ print('-Polynomail Regression:')
 print('Mean Square Error', metrics.mean_squared_error(Y_test, prediction))
 print('Accuracy', "%.4f" % (metrics.r2_score(Y_test, prediction)), '\n')
 
-for col in X_train:
-    # Fitting Polynomial Regression to the dataset
-    X=np.array(X_train[col]).reshape(-1, 1)
-    Y=np.array(Y_train).reshape(-1, 1)
-    poly = PolynomialFeatures(degree=2)
-    X_poly = poly.fit_transform(X)
-    poly.fit(X_poly, Y)
-    lin2 = LinearRegression()
-    lin2.fit(X_poly, Y)
-    # Visualising the Polynomial Regression results
-    plt.scatter(X, Y, color='blue')
-
-    plt.plot(X, lin2.predict(poly.fit_transform(X)), color='red')
-    plt.title('Polynomial Regression')
-    plt.xlabel(col)
-    plt.ylabel('Average User Rating')
-
-    plt.show()
+# for col in X_train:
+#     # Fitting Polynomial Regression to the dataset
+#     X=np.array(X_train[col]).reshape(-1, 1)
+#     Y=np.array(Y_train).reshape(-1, 1)
+#     poly = PolynomialFeatures(degree=2)
+#     X_poly = poly.fit_transform(X)
+#     poly.fit(X_poly, Y)
+#     lin2 = LinearRegression()
+#     lin2.fit(X_poly, Y)
+#     # Visualising the Polynomial Regression results
+#     plt.scatter(X, Y, color='blue')
+#
+#     plt.plot(X, lin2.predict(poly.fit_transform(X)), color='red')
+#     plt.title('Polynomial Regression')
+#     plt.xlabel(col)
+#     plt.ylabel('Average User Rating')
+#
+#     plt.show()
 
 """Gradient Boosting Reg"""
 est = GradientBoostingRegressor(n_estimators=46, max_depth=3)
