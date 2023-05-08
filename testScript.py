ed3cd965f5effca017aec727d53f11d527ff7ba3
TasneemMagdy2002 <131301862+TasneemMagdy2002@users.noreply.github.com>
2023-05-08 23:29:42 +0300

    Update testScript.py

--- testScript.py
+++ testScript.py
@@ -51,3 +51,24 @@ testData = testPre.primary_test(testData)
 testData = testPre.genres_test(testData, preFun.genresFreq, preFun.genres)
 testData = testPre.dates_test(testData)
 
+
+testData = testPre.scaler(testData, 'Developer')
+testData = testPre.scaler(testData, 'Price')
+testData = testPre.scaler(testData, 'Size')
+testData = testPre.scaler(testData, 'User Rating Count')
+
+for row in testData['Description']:
+    row = word_tokenize(row)
+    row = [preFun.remove_NewLine(i) for i in row]
+    row = [i for i in row if i == re.sub(r'//', '', i)]
+    row = [i for i in row if i == re.sub(r'https', '', i)]
+    row = [re.sub(r'[^a-zA-Z0-9\s]+', '', preFun.remove_punc(i)) for i in row]
+    row = [preFun.remove_numbers(i) for i in row]
+    row = [word for word in row if word not in stop_words]
+    row = [i for i in row if i != '']
+    row = [stemmer.stem(word) for word in row]
+    row = [lemmatizer.lemmatize(word, pos='v') for word in row]
+    DesColtest.append(len(set(row)))
+
+testData['Description']= DesColtest
+testData = testPre.scaler(testData,'Description')
