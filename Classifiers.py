f51beb54c69fa326c3830ab36585dd14d7603ea6
rwansameh2001 <71217879+rwansameh2001@users.noreply.github.com>
2023-05-11 00:35:09 +0300

    Add files via upload

--- Classifiers.py
+++ Classifiers.py
@@ -5,7 +5,14 @@ from sklearn.neighbors import KNeighborsRegressor
 from sklearn.metrics import mean_squared_error
 from sklearn.metrics import accuracy_score
 from sklearn.tree import DecisionTreeClassifier
-
+from sklearn.feature_selection import RFE, SelectKBest, f_classif
+import numpy as np
+import matplotlib.pyplot as plt
+import sklearn.linear_model
+from sklearn.feature_selection import RFE, SelectKBest, f_classif
+from sklearn.linear_model import LogisticRegression
+from sklearn import preprocessing
+from sklearn import utils
 ##################################Loading Data##################################
 X_Train = pd.read_csv('PreprocessedTrain.csv')
 Y_Train = pd.read_csv('TrainData.csv')
@@ -15,9 +22,38 @@ Y_Test = pd.read_csv('TestData.csv')
 Y_Test = Y_Test['Rate']
 print(Y_Train)
 print(X_Train)
-"""Decision Tree"""
+
+
 d = {'High': 2, 'Intermediate': 1, 'Low': 0}
 Y_Train = Y_Train.map(d)
+
+
+
+
+#feature selection using analysis of variance f_test
+# configure to select all features
+fs = SelectKBest(score_func=f_classif, k='all')
+# learn relationship from training data
+fs.fit(X_Train,Y_Train)
+
+droppedIndices=[]
+for i in range(len(fs.scores_)):
+ print('Feature %d: %f' % (i, fs.scores_[i]))
+ if(fs.scores_[i]<1):
+  droppedIndices.append(i)
+# plot the scores
+plt.bar([i for i in range(len(fs.scores_))], fs.scores_)
+plt.show()
+
+print(droppedIndices)
+X_Train = X_Train.drop(X_Train.columns[[droppedIndices]],axis = 1)
+X_Test= X_Test.drop(X_Test.columns[[droppedIndices]],axis = 1)
+
+print(X_Train.shape)
+
+
+##decision trees
+
 dtree = DecisionTreeClassifier(max_leaf_nodes=4)
 dtree = dtree.fit(X_Train, Y_Train)
 Y_Test = Y_Test.map(d)
